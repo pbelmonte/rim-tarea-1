a04c23f1d267fbf69477601ea0ed8825b9dc3b41
Pedro Belmonte <pedro.belmonte@ing.uchile.cl>
2018-10-13 19:27:56 -0300

    k vecinos para cada frame del video

--- busqueda.cpp
+++ busqueda.cpp
@@ -4,6 +4,8 @@
 
 #include "utils.h"
 
+const int k_vecinos = 3; // numero de vecinos mas cercanos de cada frame
+
 // Generar un vector con todos los comerciales
 std::vector<Video> getComerciales(const std::string &dirname) {
 
@@ -28,6 +30,39 @@ std::vector<Video> getComerciales(const std::string &dirname) {
     return videos;
 }
 
+// Juntar todos los frames de los comerciales
+std::vector<Frame> getFramesComerciales(std::vector<Video> comerciales) {
+    std::vector<Frame> frames;
+    for (const Video &comercial : comerciales) {
+        for (const Frame &frms : comercial.getFrames()) {
+            frames.push_back(frms);
+        }
+    }
+    return frames;
+}
+
+// Guardar distancias entre frames
+void getDistancias(std::vector<Frame> &videoFrames, const std::vector<Frame> &comercialesFrames) {
+    for (Frame& videoFrame : videoFrames) {
+        for (const Frame &comercialFrame : comercialesFrames) {
+            int distancia = videoFrame.distancia(comercialFrame);
+            videoFrame.addVecino(comercialFrame, distancia);
+        }
+    }
+
+    /*for (Frame videoFrame : videoFrames) {
+        std::set<std::pair<Frame, int>, Frame::comp> vecinos = videoFrame.getVecinos();
+        std::cout << vecinos.size() << std::endl;
+        std::cout << videoFrame.getVideoName() << " " << videoFrame.getNumero() << " ";
+        for (std::pair<Frame, int> par : vecinos) {
+            std::cout << "|";
+            std::cout << std::get<0>(par).getVideoName() << " " << std::get<0>(par).getNumero() << " ";
+            std::cout << std::get<1>(par);
+        }
+        std::cout << std::endl;
+    }*/
+}
+
 int main(int argc, char *argv[]) {
 
     if (argc != 3) {
@@ -42,7 +77,9 @@ int main(int argc, char *argv[]) {
 
     std::vector<Video> comerciales = getComerciales(dirname);
 
-    std::vector<Frame> vec = video.getFrames();
+    std::vector<Frame> videoFrames = video.getFrames();
+
+    getDistancias(videoFrames, getFramesComerciales(comerciales));
 
     return 0;
 }
